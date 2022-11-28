39a8c9005c7222a3e03a4cf3e14ba239611aa867
Kat Chudy <kec21@fsu.edu>
2022-11-28 13:16:11 -0500

    correct points on poly for introscreen

--- introscreen.js
+++ introscreen.js
@@ -10,21 +10,26 @@ img = loadImage('introscreen.jpg')
 
 
 this.enter = function() {
-  poly[0] = createVector(137,385);
-  poly[1] = createVector(180,362);
-  poly[2] = createVector(176,286);
-  poly[3] = createVector(253,285);
+  poly[0] = createVector(310,757);
+  poly[1] = createVector(694,761);
+  poly[2] = createVector(694,928);
+  poly[3] = createVector(309,929);
   console.log("introscreen")
 }
 
 this.draw = function() {
   background(255);
   image(img, 0, 0);
+  //beginShape();
+  //for (const {x,y} of poly) vertex(x,y);
+  //endShape(CLOSE);
 }
 
 this.mouseClicked = function() {
   hit = collidePointPoly(mouseX,mouseY,poly);
+  console.log("made it into mouse clicked")
   if (hit) {
+    console.log("hit");
     mgr.showScene(bodymap);
   }
 }
