0e10a05f919519f5e78e171c5ec07d55ade819f5
Kat Chudy <kec21@fsu.edu>
2022-11-29 17:00:43 -0500

    removed this. from some buttons

--- feedbackloop.js
+++ feedbackloop.js
@@ -2,68 +2,68 @@ function labwork() {
   console.log("labwork");
   let button4 = createButton("YOUR SPECIALIST HAS ORDERED LABWORK");
   button4.id("labworkButton");
-  button4.mousePressed(this.scan1);
+  button4.mousePressed(scan1);
 }
 
 function labwork2() {
   let button5 = createButton("YOUR SPECIALIST HAS ORDERED LABWORK");
   button5.id("labwork2Button");
-  button5.mousePressed(this.scan2);
+  button5.mousePressed(scan2);
 }
 
 function labwork3() {
   let button6 = createButton("YOUR SPECIALIST HAS ORDERED LABWORK");
   button6.id("labwork3Button");
-  button6.mousePressed(this.scan3);
+  button6.mousePressed(scan3);
 }
 
 function scan1() {
   let button7 = createButton("YOUR LABS ARE NORMAL, YOUR SPECIALIST HAS ORDERED A SCAN");
   button7.id("scan1Button");
   button7.position(random(width),random(height));
-  button7.mousePressed(this.normal1);
+  button7.mousePressed(normal1);
 }
 
 function scan2() {
   let button8 = createButton("YOUR LABS ARE NORMAL, YOUR SPECIALIST HAS ORDERED A SCAN");
   button8.id("scan2Button");
   button8.position(random(width),random(height));
-  button8.mousePressed(this.normal2);
+  button8.mousePressed(normal2);
 }
 
 function scan3() {
   let button9 = createButton("YOUR LABS ARE NORMAL, YOUR SPECIALIST HAS ORDERED A SCAN");
   button9.id("scan3Button");
   button9.position(random(width),random(height));
-  button9.mousePressed(this.normal3);
+  button9.mousePressed(normal3);
 }
 
 function normal1() {
   let button10 = createButton("YOUR SCAN WAS INCONCLUSIVE, GO BACK TO THE DOCTOR");
   button10.id("normal1");
   button10.position(random(width),random(height));
-  button10.mousePressed(this.endlesscycle1);
+  button10.mousePressed(endlesscycle1);
 }
 
 function normal2() {
   let button11 = createButton("YOUR SCAN WAS NORMAL, YOU ARE HEALTHY");
   button11.id("normal2");
   button11.position(random(width),random(height));
-  button11.mousePressed(this.endlesscycle3);
+  button11.mousePressed(endlesscycle3);
 }
 
 function normal3() {
   let button12 = createButton("YOUR SCAN WAS NORMAL, GO BACK TO THE DOCTOR");
   button12.id("normal3");
   button12.position(random(width),random(height));
-  button12.mousePressed(this.resetSketch);
+  button12.mousePressed(resetSketch);
 }
 
 function endlesscycle1() {
   let button13 = createButton("GO BACK TO THE DOCTOR AGAIN AND START THE PROCESS OVER");
   button13.id("endlesscycle1");
   button13.position(random(width),random(height));
-  button13.mousePressed(this.resetSketch);
+  button13.mousePressed(resetSketch);
 }
 
 function resetSketch() {
